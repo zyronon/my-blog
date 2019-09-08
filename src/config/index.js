149751b6f3db55b8855c12519abe30b0cf93ff7b
ttentau <o8949488816>
2019-09-09 00:38:45 +0800

    bf

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
     IS_DEV: true,
-    API_URL: 'http://www.ttentau.com/',
-    PRODUCT_API_URL: 'http://www.ttentau.top:12345/',
+    API_URL: 'http://blogapi.ttentau.com',
+    PRODUCT_API_URL: 'http://www.ttentau.top:12345',
     WS_URL: 'ws://www.ttentau.com:2346',
     PRODUCT_WS_URL: 'ws://www.ttentau.top:2346',
 
