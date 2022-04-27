25d556550e97d291573c4ea1a06de9430aa9ebaa
超哥 <ttentau@163.com>
2022-04-28 01:58:28 +0800

    bf

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Home from './views/home/Home'
+import Archive from './views/archive/Archive'
+import Article from "./views/article/Article";
+import About from "./views/about/About";
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/archive" element={<Archive/>}/>
+      <Route path="/article" element={<Article/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+  </BrowserRouter>
+</React.StrictMode>)
